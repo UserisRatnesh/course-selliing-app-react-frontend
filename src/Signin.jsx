d253762c4ed42adac69959065c710500512c4c8a
Ratnesh singh <ratneshsingh@Ratneshs-MacBook-Air.local>
2023-07-29 17:10:09 +0530

    commit one

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -2,8 +2,12 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import {Typography} from "@mui/material";
+import { useState } from "react";
 
 function Signin(){
+    const [email, setEmail]= useState("");
+    const [password, setPassword] = useState("");
+    
     return <div >
         <div style={{
             paddingTop:150,
@@ -12,7 +16,7 @@ function Signin(){
             justifyContent: "center"
         }}>
             <Typography variant={"h6"}>
-                Welcome back. Sign In below!
+                Welcome back. Sign Up below!
             </Typography>
         </div>
         <div style={{
@@ -23,11 +27,44 @@ function Signin(){
                 width: 400,
                 padding: 20
                 }}>
-                <TextField fullWidth={true} id="username" label="Email" variant="outlined" />
+                <TextField 
+                    onChange = {(e)=>{
+                        setEmail(e.target.value);
+                    }}
+                    fullWidth={true} 
+                    label="Email" 
+                    variant="outlined" />
                 <br/><br/>
-                <TextField fullWidth={true} type="password" id="password" label="Password" variant="outlined" />
+                <TextField 
+                    onChange = {(e)=>{
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true} 
+                    type="password" 
+                    label="Password" 
+                    variant="outlined" />
                 <br/><br/>
-                <Button size={"large"} variant="contained">Sign In</Button>
+                <Button 
+                    size={"large"}
+                    variant="contained"
+                    onClick={()=>{
+                        fetch("http://localhost:3000/admin/signin",{
+                            method : "POST",
+                            body : JSON.stringify({
+                                username : email,
+                                password : password
+                            }),
+                            headers : {
+                                "Content-type" : "application/json"
+                            }
+                        }).then((res)=>{
+                            res.json().then((data)=>{
+                                localStorage.setItem("token",data.token);
+                                console.log(data);
+                            })
+                        });
+                    }}
+                    >Sign In</Button>
             </Card>
         </div>
     </div>
